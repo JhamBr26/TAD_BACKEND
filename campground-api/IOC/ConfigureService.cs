054423eddff76fb4500fb1965dbc2d0f6090585f
Dreamlocked <leonardo.olae.saa@gmail.com>
2024-01-20 12:21:53 -0500

    fix(api): Mejora de seguridad

---
+++ campground-api/IOC/ConfigureService.cs
@@ -0,0 +1,82 @@
+﻿
+using campground_api.Models;
+using campground_api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
+namespace campground_api.IOC
+{
+    public static class ServiceConfiguration
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("JwtKey") ?? builder.Configuration["Jwt:Key"]!)),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+
+                o.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        context.Token = context.Request.Cookies[builder.Configuration["Jwt:CookieName"]!];
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Campground",
+                    Version = "v1.0.1"
+                });
+            });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins",
+                    builder =>
+                    {
+                        builder.WithOrigins(Environment.GetEnvironmentVariable("Frontend") ?? "http://localhost")
+                            .AllowAnyHeader()
+                            .AllowCredentials()
+                            .AllowAnyMethod();
+                    });
+            });
+
+            builder.Services.AddDbContext<CampgroundContext>(
+                options =>
+                   options.UseSqlServer(Environment.GetEnvironmentVariable("DbContext") ?? builder.Configuration.GetConnectionString("DbContext")));
+
+            builder.Services.AddScoped<CampgroundService>();
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<ReviewService>();
+
+            builder.Services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+            });
+        }
+    }
+}
--- campground-api/Program.cs
+++ campground-api/Program.cs
@@ -8,90 +8,15 @@ using Microsoft.OpenApi.Models;
 using System.Text;
 using System.Text.Json.Serialization;
 using Azure.Identity;
+using campground_api.IOC;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = false,
-        ValidateIssuerSigningKey = true
-    };
-    o.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            context.Token = context.Request.Cookies[builder.Configuration["Jwt:CookieName"]];
-            return Task.CompletedTask;
-        }
-    };
-});
-builder.Services.AddAuthorization();
-// Add configuration from appsettings.json
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen( option =>
-{
-    option.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Campground",
-        Version = "v1.0.1"
-    });
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please insert JWT with Bearer into field",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-        builder =>
-        {
-            builder.WithOrigins("https://campgrounds-frontend.vercel.app/", "http://localhost")
-                .AllowAnyHeader()
-                .AllowCredentials()
-                .AllowAnyMethod();
-        });
-});
-
-
-builder.Services.AddDbContext<CampgroundContext>(
-    options =>
-       options.UseSqlServer(builder.Configuration.GetConnectionString("DbContext")));
-
-builder.Services.AddScoped<CampgroundService>();
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<ReviewService>();
-
-builder.Services.Configure<RouteOptions>(options =>
-{
-    options.LowercaseUrls = true;
-});
+builder.ConfigureServices();
 
 var app = builder.Build();
 
